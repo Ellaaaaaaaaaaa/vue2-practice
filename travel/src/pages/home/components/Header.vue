b9473ecf7a2e8fb0dfb4da224d6414d332ce8808
Ellaaaa <2568366315@qq.com>
2024-01-23 09:35:47 +0800

    city-header

--- travel/src/pages/home/components/Header.vue
+++ travel/src/pages/home/components/Header.vue
@@ -1,61 +1,58 @@
 <template>
     <div class="header">
       <div class="header-left">
-        <div class="iconfont back-icon">&#xe624;</div>
+        <div class="iconfont back-icon">&#xe61e;</div>
       </div>
       <div class="header-input">
-        <span class="iconfont">&#xe632;</span>
+        <span class="iconfont">&#xe6a2;</span>
         输入城市/景点/游玩主题
       </div>
-      <router-link to='/city'>
+      <router-link to="/city">
         <div class="header-right">
           {{this.city}}
-          <span class="iconfont arrow-icon">&#xe64a;</span>
+          <span class="iconfont arrow-icon">&#xe621;</span>
         </div>
       </router-link>
     </div>
   </template>
-
 <script>
-import { mapState } from 'vuex'
 export default {
   name: 'HomeHeader',
-  computed: {
-    ...mapState(['city'])
+  props: {
+    city: String
   }
 }
 </script>
-
-  <style lang="stylus" scoped>
-    @import '~styles/varibles.styl'
-    .header
-      display: flex
-      line-height: $headerHeight
-      background: $bgColor
-      color: #fff
-      .header-left
-        width: .64rem
-        float: left
-        .back-icon
-          text-align: center
-          font-size: .4rem
-      .header-input
-        flex: 1
-        height: .64rem
-        line-height: .64rem
-        margin-top: .12rem
-        margin-left: .2rem
-        padding-left: .2rem
-        background: #fff
-        border-radius: .1rem
-        color: #ccc
-      .header-right
-        min-width: 1.04rem
-        padding: 0 .1rem
-        float: right
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header
+    display: flex
+    line-height: $headerHeight
+    background: $bgColor
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+      .back-icon
         text-align: center
-        color: #fff
-        .arrow-icon
-          margin-left: -.04rem
-          font-size: .24rem
-  </style>
+        font-size: .4rem
+    .header-input
+      flex: 1
+      height: .64rem
+      line-height: .64rem
+      margin-top: .12rem
+      margin-left: .2rem
+      padding-left: .2rem
+      background: #fff
+      border-radius: .1rem
+      color: #ccc
+    .header-right
+      min-width: 1.04rem
+      padding: 0 .1rem
+      float: right
+      text-align: center
+      color: #fff
+      .arrow-icon
+        margin-left: -.04rem
+        font-size: .24rem
+</style>
--- travel/src/pages/home/components/Swiper.vue
+++ travel/src/pages/home/components/Swiper.vue
@@ -19,8 +19,7 @@ export default {
     return {
       swiperOption: {
         pagination: '.swiper-pagination',
-        loop: true,
-        autoplay: 3000
+        loop: true
       }
     }
   },
